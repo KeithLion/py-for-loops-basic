e125a273e621514731f77dbccedf524f4b916b9e
KeithLion <nortonkiara6@gmail.com>
2021-11-30 16:46:26 -0800

    added for loop that prints, coding for multiples of , and coding dojo for 10 or 5.

--- for_loops_basic1.py
+++ for_loops_basic1.py
@@ -3,5 +3,13 @@
 #     print(y)
 #     y += 1
 
-for i in range(0, 200):
-    print(5*i)
+# for i in range(0, 200):
+#     print(5*i)
+
+# for i in range(0, 100):
+#     if i % 5 == 0:
+#         print('coding')
+#     if i % 10 == 0 or i % 5 == 0:
+#         print('coding dojo')
+#     else:
+#         print(i)
